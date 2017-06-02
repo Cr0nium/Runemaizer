c79a1510423eb66ae09c8a021b724a8825eaced4
Cr0nium <timur.zalimov@gmail.com>
2017-06-02 14:24:52 +0300

    create add_form

---
+++ src/swrunes/rune.java
@@ -0,0 +1,30 @@
+package swrunes;
+
+
+public class rune {
+    
+  private int id;
+  private String set;
+  private int slot;
+  private String ms;
+  private int msv;
+  private String ps;
+  private int psv;
+  private int atkp;
+  private int atps;
+  private int cri_r;
+  private int cri_d;
+  private int spd;
+  private int hpp;
+  private int hps;
+  private int defp;
+  private int defs;
+  private int accp;
+  private int resp;
+
+    
+    public static void main(String[] args) {
+       
+    }
+    
+}
